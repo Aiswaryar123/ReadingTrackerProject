09d05bbddffd5d4daeeea07dcda431944efee36a
Aiswarya r <aiswaryarathee123@gmail.com>
2025-12-24 12:07:05 +0530

    implement user handler for registration and login endpoints

---
+++ Backend/Internal/handlers/user_handler.go
@@ -0,0 +1,64 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/Aiswaryar123/ReadingTrackerProject/Internal/dto"
+	"github.com/Aiswaryar123/ReadingTrackerProject/Internal/services"
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	service services.UserService
+}
+
+func NewUserHandler(service services.UserService) *UserHandler {
+	return &UserHandler{service: service}
+}
+
+func (h *UserHandler) Register(c *gin.Context) {
+	var req dto.RegisterRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := h.service.Register(req)
+	if err != nil {
+
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	response := dto.UserResponse{
+		ID:    user.ID,
+		Name:  user.Name,
+		Email: user.Email,
+	}
+	c.JSON(http.StatusCreated, response)
+}
+
+func (h *UserHandler) Login(c *gin.Context) {
+	var req dto.LoginRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := h.service.Login(req)
+	if err != nil {
+
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	response := dto.UserResponse{
+		ID:    user.ID,
+		Name:  user.Name,
+		Email: user.Email,
+	}
+	c.JSON(http.StatusOK, response)
+}
